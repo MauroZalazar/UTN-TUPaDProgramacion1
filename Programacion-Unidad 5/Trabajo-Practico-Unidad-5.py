89ef9e7bfe497824f50f5dff5e3135edc90838c4
MauroZalazar <maurodrobeck67@gmail.com>
2026-09-19 21:53:48 -0300

    Trabajo Practico Unidad 5

---
+++ Programacion-Unidad 5/Trabajo-Practico-Unidad-5.py
@@ -0,0 +1,84 @@
+#Punto 1
+
+print("\n--- Ejercicio 1 ---")
+input("Presiona Enter para continuar...")
+multiplos_cuatro = list(range(4, 101, 4))
+print(multiplos_cuatro)
+
+#Punto 2
+
+print("\n--- Ejercicio 2 ---")
+input("Presiona Enter para continuar...")
+elementos = ["python", "GTA6", "minecraft", "computadora", "vegeta777"]
+penultimo = elementos[-2]
+print(penultimo)
+
+#Punto 3
+print("\n--- Ejercicio 3 ---")
+input("Presiona Enter para continuar...")
+lista_vacia = []
+lista_vacia.append("manzana")
+lista_vacia.append("pera")
+lista_vacia.append("banana")
+print(lista_vacia)
+
+#Punto 4
+
+print("\n--- Ejercicio 4 ---")
+input("Presiona Enter para continuar...")
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[-1] = "oso"
+print(animales)
+
+#Punto 5
+
+print("\n--- Ejercicio 5 ---")
+input("Presiona Enter para continuar...")
+numeros = [8, 15, 3, 22, 7]
+numeros.remove(max(numeros))
+print(numeros)
+
+#Punto 6
+
+print("\n--- Ejercicio 6 ---")
+input("Presiona Enter para continuar...")
+numeros_salto = list(range(10, 31, 5))
+print(numeros_salto[0], numeros_salto[1])
+
+#Punto 7
+
+print("\n--- Ejercicio 7 ---")
+input("Presiona Enter para continuar...")
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "audi"
+autos[2] = "fiat"
+print(autos)
+
+#Punto 8
+
+print("\n--- Ejercicio 8 ---")
+input("Presiona Enter para continuar...")
+dobles = []
+dobles.append(5 * 2)
+dobles.append(10 * 2)
+dobles.append(15 * 2)
+print(dobles)
+
+#Punto 9
+
+print("\n--- Ejercicio 9 ---")
+input("Presiona Enter para continuar...")
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+
+#Punto 10
+
+print("\n--- Ejercicio 10 ---")
+input("Presiona Enter para continuar...")
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print(lista_anidada)
+
